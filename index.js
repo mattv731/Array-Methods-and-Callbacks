4a97ae64312ae9e89fbd4858ea32daa76b032e49
abc <abc@example.com>
2021-10-12 11:46:17 -0600

    Finished Task 1

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ const { fifaData } = require('./fifa.js')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-
+const finals2014 = fifaData.filter(function(item){
+    return item['Year'] === 2014 && item['Stage'] === "Final";
+});
+console.log(finals2014)
 //(a) Home Team name for 2014 world cup final
-
+console.log('Task 1a', finals2014[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
+console.log('Task 1b', finals2014[0]['Away Team Name']);
 
 //(c) Home Team goals for 2014 world cup final
-
+console.log('Task 1c', finals2014[0]['Home Team Goals']);
 //(d) Away Team goals for 2014 world cup final
+console.log('Task 1d', finals2014[0]['Away Team Goals']);
 
 //(e) Winner of 2014 world cup final */
-
+console.log('Task 1e', finals2014[0]['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -49,6 +54,7 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
 
+// Don't care about over time, just return points.
 function getWinners(/* code here */) {
     /* code here */
 }
@@ -76,6 +82,7 @@ Use the higher order function getAverageGoals to do the following:
  1. Receive the callback function getFinals from task 2 ensure you pass in the data as an argument
  2. Return the the average number of the total home team goals and away team goals scored per match and round to the second decimal place. 
  
+ .toFixed(2)
  (Hint: use .reduce and do this in 2 steps) 
  
  Example of invocation: getAverageGoals(getFinals(fifaData));
